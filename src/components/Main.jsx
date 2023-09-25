3b0c5a7aa41110a963e98e270cc9d1b17bdd41d8
Rodri.V <veronrodrigo98@gmail.com>
2023-09-25 12:32:07 -0300

    se modifico la pagina principal

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ export const Main = () => {
     return (
         <main>
             <div className="container">
-                <div className="row">
+                <div className="row d-flex">
                     <div className="col-md-6">
                         <div className="filtros">
                             <div className="filtro1 bg-danger">
@@ -14,27 +14,22 @@ export const Main = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-6">
-                        <div className="filtros">
-                            <div className="filtro2">
-                                <div className="btn-group">
-                                    <button
-                                        className="button2 btn bg-secondary-subtle rounded-5 dropdown-toggle dropdown-toggle-split d-inline-flex align-items-center justify-content-center"
-                                        type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        SELECCIONAR CINE
-                                    </button>
+                    <div className="filtros col-md-6 d-flex">
+                        <div className="btn-group">
+                            <button
+                                className="button2 btn bg-secondary-subtle rounded-5 dropdown-toggle dropdown-toggle-split d-inline-flex align-items-center justify-content-center"
+                                type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                SELECCIONAR CINE
+                            </button>
 
-                                    <ul className="dropdown-menu bg-secondary-subtle text-center">
-                                        <li>
-                                            <a href="" className="dropdown-items p-2">Cines Avenida</a>
-                                        </li>
-                                        <li>
-                                            <a href="" className="dropdown-items p-2">Cine Teatro Italia</a>
-                                        </li>
-                                    </ul>
-
-                                </div>
-                            </div>
+                            <ul className="dropdown-menu bg-secondary-subtle text-center">
+                                <li>
+                                    <a href="" className="dropdown-items p-2">Cines Avenida</a>
+                                </li>
+                                <li>
+                                    <a href="" className="dropdown-items p-2">Cine Teatro Italia</a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
